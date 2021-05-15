57b560107127481db2a0bcb295a9eb7781d33550
Seun Fapohunda <seunfapps@gmail.com>
2021-05-15 21:18:31 +0100

    Array removeAt, indexOf

--- src/main/java/com/seunfapps/algos/datastructures/Array.java
+++ src/main/java/com/seunfapps/algos/datastructures/Array.java
@@ -3,6 +3,9 @@ package com.seunfapps.algos.datastructures;
 public class Array {
     private int [] items;
     private int count; //keeps track of index that actually have values set.
+    public int getCount() {
+        return count;
+    }
 
     public Array(int length){
         items = new int [length];
@@ -12,8 +15,27 @@ public class Array {
         count++;
     }
 
+    public void removeAt(int index){
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        for (int i = index; i < count; i++){
+            items[i] = items[i+1];
+        }
+        count--;
+    }
+
+    public int indexOf(int item){
+        for(int i = 0; i < count; i++){
+            if(items[i] == item)
+                return i;
+        }
+        return -1;
+    }
     public void print(){
+        System.out.print("[");
         for(int i = 0; i < count; i++)
-            System.out.println(items[i]);
+            System.out.print(items[i]+ " ");
+        System.out.print("]");
     }
 }
--- src/test/java/com/seunfapps/tests/ArrayTests.java
+++ src/test/java/com/seunfapps/tests/ArrayTests.java
@@ -1,14 +1,95 @@
 package com.seunfapps.tests;
 
 import com.seunfapps.algos.datastructures.Array;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArrayTests {
+
     @Test
     public void shouldInsertIntoArray(){
         Array array = new Array(5);
         array.insert(20);
         array.insert(30);
         array.print();
+        Assertions.assertEquals(array.getCount(), 2);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfIndexIsLessThanZero(){
+        Array array = new Array(5);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           array.removeAt(-1);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionIfIndexIsGreaterThanCount(){
+        Array array = new Array(5);
+        array.insert(20);
+        array.insert(30);
+        array.insert(35);
+        array.print();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            array.removeAt(4);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionIfIndexIsEqualToCount(){
+        Array array = new Array(5);
+        array.insert(20);
+        array.insert(30);
+        array.insert(35);
+        array.print();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            array.removeAt(3);
+        });
+    }
+
+    @Test
+    public void shouldRemoveItemFromArray(){
+        Array array = new Array(5);
+        array.insert(20);
+        array.insert(30);
+        array.insert(35);
+        array.insert(15);
+        array.print();
+        int beforeCount = array.getCount();
+        array.removeAt(1);
+        array.print();
+        int afterCount = array.getCount();
+
+        Assertions.assertEquals(afterCount, beforeCount - 1);
+
+    }
+
+    @Test
+    public void shouldReturnNegativeOneIfNumberNotFound(){
+        Array array = new Array(5);
+        array.insert(20);
+        array.insert(30);
+        array.insert(35);
+        array.insert(15);
+
+        int index = array.indexOf(69);
+        Assertions.assertEquals(index,  - 1);
+
+    }
+
+    @Test
+    public void shouldReturnIndexIfFound(){
+        Array array = new Array(5);
+        array.insert(20);
+        array.insert(30);
+        array.insert(35);
+        array.insert(15);
+
+        int index = array.indexOf(35);
+        Assertions.assertEquals(index,  2);
+
     }
 }
